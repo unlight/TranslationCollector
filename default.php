fbcd50076998ba65ea2a67c4c67aa0fd93f03119
unknown <unlights@gmail.com>
2010-08-28 18:44:48 +0400

    First commit

---
+++ default.php
@@ -0,0 +1,115 @@
+<?php if (!defined('APPLICATION')) exit();
+
+/*
+1. DESCRIPTION
+==============
+1. Plugin catch undefined translate codes and save it 
+(Make sure for exists event BeforeTranslate ~ line 200 in /library/core/class.locale.php)
+2. Generates dummy locale for all applications and plugins (see generate-dummy-locale.php)
+
+2. INSTALL
+==========
+Make sure for exists event BeforeTranslate ~ line 200 in /library/core/class.locale.php
+If it is not, add lines:
+$this->EventArguments['Code'] = $Code;
+$this->FireEvent('BeforeTranslate');
+Before this line:
+if (array_key_exists($Code, $this->_Definition)) {
+
+3. USAGE
+========
+Enable plugin in dashboard, and see plugins directory /plugins/TranslationCollector
+Translation codes will be saved to file [ApplicationName].php
+[!] Already translated codes (definitions which are exists in locale files) will NOT be saved.
+
+4. KNOWN ISSUES
+===============
+If you get error: syntax error, unexpected T_STRING, expecting ']' in
+/some/path/to/plugins/TranslationCollector/[ApplicationName].php 
+If you get this error remove line or delete this file.
+This is issue fixed in version 1.1
+
+5. CHANGELOG
+============
+28 Aug 2010 / 1.3
+[fix] http://github.com/vanillaforums/Garden/issues/issue/497
+[alt] non *handler methods made protected [no need check it by PluginManager::RegisterPlugins()]
+[add] default translate for english codes eg. EmailWelcome, PasswordRequest, etc.
+[add] collect codes from form methods (Label, Button, etc.)
+01 Aug 2010 / 1.2
+[new] dummy locale generator
+[alt] undefined definitions moved to directory "undefined"
+[add] byte order mask for saved files
+19 Jun 2010 / 1.1
+[fix] fixed issue (syntax error, unexpected T_STRING, expecting ']')
+[alt] default application Vanilla to Dashboard
+17 Jun 2010 / 1.0
+[new] first release
+
+6. TODO
+=======
+option skip already translated strings
+fix warning: Unterminated comment starting
+Better detection default code // $Definition['Theme_$Key'] = '';
+console param to collect codes from desired application/plugin
+how we can catch this array_map('T', array())?
+remove garbage: if (!$this->t->isDone()) minify plugin
+*/
+
+
+$PluginInfo['TranslationCollector'] = array(
+	'Name' => 'Translation collector',
+	'Description' => 'Collects undefined translation codes and save it for translating.',
+	'Version' => '1.3',
+	'Date' => '28 Aug 2010'
+);
+
+class TranslationCollectorPlugin implements Gdn_IPlugin {
+	
+	private $_Definition = array();
+	
+	public function __construct(){
+		$Locale = Gdn::Locale();
+		$Export = var_export($Locale, 1);
+		if(preg_match("/   '_Definition' => ((.+?)\n  \)),/s", $Export, $Match)){
+			if(isset($Match[1])) eval("\$this->_Definition = $Match[1];");
+		}
+	}
+	
+	protected function Translate($Code){
+		return ArrayValue($Code, $this->_Definition);
+	}
+	
+	public function Gdn_Locale_BeforeTranslate_Handler(&$Sender) {
+		$Code = ArrayValue('Code', $Sender->EventArguments, '');
+		if(array_key_exists($Code, $this->_Definition)) return;
+		$File = CombinePaths(array(dirname(__FILE__), 'undefined', $this->_EnabledApplication().'.php'));
+		$HelpText = 'TRANSLATE, CUT AND PASTE THIS TO /applications/application-folder/locale/locale-name-folder/definitions.php';
+		$HelpText .= '\xEF\xBB\xBF'; // UTF-8 byte order mask
+		if(!file_exists($File)) Gdn_FileSystem::SaveFile($File, "<?php // $HelpText\n");
+		$Definition = array();
+		include $File;
+		if(!array_key_exists($Code, $Definition)){
+			$FileContent = file_get_contents($File);
+			$Code = var_export($Code, True); // should be escaped
+			$FileContent .= "\n\$Definition[".$Code."] = $Code;";
+			Gdn_FileSystem::SaveFile($File, $FileContent);
+		}
+	}
+
+	private $_EnabledApplication = 'Dashboard';
+		
+	public function Gdn_Dispatcher_AfterEnabledApplication_Handler(&$Sender) {
+		$this->_EnabledApplication = ArrayValue('EnabledApplication', $Sender->EventArguments, 'Dashboard');
+	}
+	
+	private function _EnabledApplication() {
+		return $this->_EnabledApplication;
+	}
+	
+	public function Setup(){
+	}
+	
+	
+	
+}
---
+++ generate-dummy-locale.php
@@ -0,0 +1,134 @@
+#!/usr/local/bin/php
+<?php
+/*
+1. DESCRIPTION
+==============
+Script generates default locale files for all applications and plugins
+
+2. USAGE
+========
+1. Delete locale_map.ini from cache directory
+2. Run script: php -q generate-dummy-locale.php
+
+*/
+
+error_reporting(E_ALL);
+ini_set('html_errors', 'Off');
+ini_set('display_errors', 'On');
+ini_set('track_errors', 'On');
+
+if(!defined('DS')) define('DS', '/');
+chdir(dirname(__FILE__).DS.'..'.DS.'..');
+if(!defined('PATH_ROOT')) define('PATH_ROOT', realpath('.'));
+if(!defined('APPLICATION')) define('APPLICATION', 'Garden');
+if(!defined('APPLICATION_VERSION')) define('APPLICATION_VERSION', '1.0');
+
+require_once PATH_ROOT.DS.'bootstrap.php';
+
+$Applications = array('Dashboard', 'Vanilla', 'Conversations');
+$Applications = array_combine($Applications, array_map('strtolower', $Applications));
+
+$T = Gdn::FactoryOverwrite(TRUE);
+$DefaultLocale = new Gdn_Locale('en-CA', $Applications, array());
+Gdn::FactoryInstall(Gdn::AliasLocale, 'Gdn_Locale', PATH_LIBRARY_CORE.DS.'class.locale.php', Gdn::FactorySingleton, $DefaultLocale);
+Gdn::FactoryOverwrite($T);
+
+
+/*$TestFile = dirname(__FILE__).DS.'_____test-ss.php';
+GetTranslationFromFile($TestFile);die;*/
+
+$Locale = array();
+$Directory = new RecursiveDirectoryIterator(PATH_ROOT);
+foreach(new RecursiveIteratorIterator($Directory) as $File){
+	$RealPath = $File->GetRealPath();
+	$Extension = strtolower(pathinfo($RealPath, 4));
+	if($Extension != 'php') continue;
+	$Path = substr($RealPath, strlen(PATH_ROOT) + 1);
+	$PathArray = explode('/', str_replace('\\', '/', $Path));
+	$StoreDirectory = 'applications/dashboard';
+	if(in_array($PathArray[0], array('applications', 'plugins'))) $StoreDirectory = $PathArray[0].'/'.$PathArray[1];
+	if(in_array($PathArray[0], array('themes'))) continue;
+	$StoreDirectory .= '/locale/zz-ZZ';
+	$StoreDirectory = dirname(__FILE__).'/dummy-locale/' . $StoreDirectory;
+	$DefinitionsFile = $StoreDirectory . '/definitions.php';
+	$Codes = GetTranslationFromFile($RealPath);
+	if(!isset($Locale[$DefinitionsFile])) $Locale[$DefinitionsFile] = array();
+	$Locale[$DefinitionsFile] = array_merge($Locale[$DefinitionsFile], $Codes);
+}
+// save it
+$Date = date('r');
+foreach($Locale as $File => $Codes){
+	if(count($Codes) == 0) continue;
+	$Directory = dirname($File);
+	if(!is_dir($Directory)) mkdir($Directory, 0777, True);
+	$FileContent = "<?php\n// Date: $Date \xEF\xBB\xBF"; // byte order mask
+	foreach($Codes as $Code => $T){
+		$Code = var_export($Code, True);
+		$T = var_export($T, True);
+		$FileContent .= "\n\$Definition[{$Code}] = $T;";
+	}
+	Gdn_FileSystem::SaveFile($File, $FileContent);
+}
+
+function GetTranslationFromFile($File){
+	$Result = array();
+	$Content = file_get_contents($File);
+	if(!$Content) return $Result;
+	$AllTokens = token_get_all($Content);
+	foreach($AllTokens as $N => $TokenData){
+		$TokenNum = ArrayValue(0, $TokenData);
+		if($TokenNum != 307) continue;
+		$FunctionName = strtolower($TokenData[1]);
+		// Form
+		$Functions = array('t', 'translate', 'plural', 'button', 'close', 'label');
+		if(!in_array($FunctionName, $Functions)) continue;
+		// find strings between () [also default code]
+		$NextTokens = array_slice($AllTokens, $N);
+		$OffSet = False;
+		$Length = False;
+		foreach($NextTokens as $N => $Token){
+			if($OffSet === False && is_string($Token) && $Token == '(') $OffSet = $N;
+			elseif($Length === False && is_string($Token) && $Token == ')') $Length = $N;
+			if($Length !== False && $OffSet !== False) break;
+		}
+		// find translation
+		$Tokens = array_slice($NextTokens, $OffSet, $Length);
+		$Strings = array();
+		foreach($Tokens as $N => $TokenData){
+			$TokenNum = ArrayValue(0, $TokenData);
+			//if($TokenNum == 309) $VariableInTranslate = True;
+			if($TokenNum != 315) continue;
+			$String = trim($TokenData[1], '\'"');
+			if(!is_numeric($String)) $Strings[] = stripslashes($String);
+		}
+		$TokenValues = array_values(array_filter($Tokens, 'is_string'));
+		$RightBracketKey = array_search(')', $TokenValues);
+		$TokenValues = array_slice($TokenValues, 1, $RightBracketKey - 1);
+		$CountTokenValues = array_count_values($TokenValues);
+		$bTernary = (in_array('?', $TokenValues) && in_array(':', $TokenValues));
+		$bPlural = ($FunctionName == 'plural');
+		$bPluralForms = ($bPlural && array_key_exists(',', $CountTokenValues) && $CountTokenValues[','] == 2);
+		$bForm = in_array($FunctionName, array('button', 'close', 'label'));
+		$bDefaultCode = (!$bForm && array_key_exists(',', $CountTokenValues) && $CountTokenValues[','] == 1 && count($Strings) == 2);
+		$bConcatenated = in_array('.', $TokenValues);
+		// save to array and return
+		foreach($Strings as $String){
+			if($bDefaultCode){
+				$Result[$String] = $Strings[1];
+				break;
+			}
+			$T = $String;
+			if($bPluralForms) $T = 'pl. '.$String;
+			$Result[$String] = T($T);
+			if($bForm) break;
+		}
+	}
+	return $Result;
+}
+
+
+
+
+
+
+
