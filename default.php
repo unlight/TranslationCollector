22cddc0d117e4626c94bcf0fb424a606dc91d0f0
S <unlights@gmail.com>
2011-06-25 10:23:27 +0400

    undefined variable

--- default.php
+++ default.php
@@ -3,7 +3,7 @@
 $PluginInfo['TranslationCollector'] = array(
 	'Name' => 'Translation Collector',
 	'Description' => 'Collects undefined translation codes and save it for translating.',
-	'Version' => '1.6.9',
+	'Version' => '1.6.10',
 	'Date' => 'Summer 2011'
 );
 
@@ -13,7 +13,7 @@ if (C('Plugins.TranslationCollector.CaptureDefinitions', True)) {
 	
 	class TranslationCollectorLocale extends Gdn_Locale {
 		
-		public function __construct($LocaleName = Null, $ApplicationWhiteList = Null, $PluginWhiteList = Null, $ForceRemapping = FALSE) {
+		public function __construct($LocaleName = Null, $ApplicationWhiteList = Null, $PluginWhiteList = Null, $ForceRemapping = False) {
 			// If called from PluginManager::GetPluginInstance()
 			// Do nothing.
 			if ($LocaleName === Null) return;
@@ -30,10 +30,10 @@ if (C('Plugins.TranslationCollector.CaptureDefinitions', True)) {
 	
 	$TcLocale = Gdn::Locale();
 	if (is_null($TcLocale)) {
-		$CurrentLocale = Gdn::Config('Garden.Locale', 'en-CA');
+		$CurrentLocale = C('Garden.Locale', 'en-CA');
 		$TcLocale = new TranslationCollectorLocale($CurrentLocale, C('EnabledApplications'), C('EnabledPlugins'));
 		$Overwrite = Gdn::FactoryOverwrite(True);
-		Gdn::FactoryInstall(Gdn::AliasLocale, 'TranslationCollectorLocale', __FILE__, Gdn::FactorySingleton, $TcLocale);
+		Gdn::FactoryInstall(Gdn::AliasLocale, 'TranslationCollectorLocale', Null, Gdn::FactorySingleton, $TcLocale);
 		Gdn::FactoryOverwrite($Overwrite);
 	}
 	
@@ -45,62 +45,60 @@ class TranslationCollectorPlugin implements Gdn_IPlugin {
 	
 	private $_Definition = array();
 	private $_EnabledApplication = 'Dashboard';
-	private $SkipApplications = array();
+	private $_SkipApplications = array();
 	
 	public function __construct() {
+		$this->_Definition = self::GetLocaleDefinitions();
+		$this->_SkipApplications = C('Plugins.TranslationCollector.SkipApplications', array());
+	}
+	
+	public static function GetLocaleDefinitions() {
 		$Locale = Gdn::Locale();
 		unset($Locale->EventArguments['WildEventStack']);
-		$Export = var_export($Locale, True);
-		$CutPointA = strpos($Export, "'_Definition' =>") + 16;
-		$CutPointB = strrpos($Export, "'_Locale' =>", $CutPointA);
-		if ($CutPointA === False || $CutPointB === False) {
-			trigger_error('Failed to detect cutpoints.', E_USER_ERROR);
-		}
-		$Match = substr($Export, $CutPointA, ($CutPointB - $CutPointA));
-		$Match = trim(trim(trim($Match), ','));
-		eval("\$this->_Definition = $Match;");
+		$Variable = var_export($Locale, True);
+		$CutPoint1 = strpos($Variable, "'_Definition' =>") + 16;
+		$CutPoint2 = strrpos($Variable, "'_Locale' =>", $CutPoint1);
+		if ($CutPoint1 === False || $CutPoint2 === False) throw new Exception('Failed to detect cutpoints.');
 		
-		$this->SkipApplications = C('Plugins.TranslationCollector.SkipApplications', array());
+		$Match = substr($Variable, $CutPoint1, ($CutPoint2 - $CutPoint1));
+		$Match = trim(trim(trim($Match), ','));
+		// Kids, never use eval.
+		eval("\$Result = $Match;");
+		return $Result;
 	}
 	
-	protected function Translate($Code) {
-		return ArrayValue($Code, $this->_Definition);
-	}
-	
-	public function TranslationCollectorLocale_BeforeTranslate_Handler(&$Sender) {
+	public function TranslationCollectorLocale_BeforeTranslate_Handler($Sender) {
 		
 		$Application = $this->_EnabledApplication();
-		if (in_array($Application, $this->SkipApplications)) return;
+		if (in_array($Application, $this->_SkipApplications)) return;
 		
 		$Code = GetValue('Code', $Sender->EventArguments, '');
 		if (substr($Code, 0, 1) == '@') return;
 		if (array_key_exists($Code, $this->_Definition)) return;
 		
-		$File = CombinePaths(array(dirname(__FILE__), 'undefined', $Application.'.php'));
-		$HelpText = 'TRANSLATE, CUT AND PASTE THIS TO /locales/locale-name-folder/definitions.php';
-		$HelpText .= "\xEF\xBB\xBF"; // utf-8 byte order mask
-		if (!file_exists($File)) Gdn_FileSystem::SaveFile($File, "<?php // $HelpText\n");
+		$File = dirname(__FILE__) . '/undefined/' . $Application . '.php';
+		$HelpText = "Translate, cut and paste this to /locales/your-locale/$Application.php";
+		if (!file_exists($File)) file_put_contents($File, "<?php // \xEF\xBB\xBF $HelpText\n");
 		$Definition = array();
 		include $File;
 		if (!array_key_exists($Code, $Definition)) {
-			$FileContent = file_get_contents($File);
-			$Code = var_export($Code, True); // should be escaped
-			$FileContent .= "\n\$Definition[".$Code."] = $Code;";
-			Gdn_FileSystem::SaveFile($File, $FileContent);
+			// Should be escaped.
+			$Code = var_export($Code, True);
+			$PhpArrayCode .= "\n\$Definition[".$Code."] = $Code;";
+			file_put_contents($File, $PhpArrayCode, FILE_APPEND | LOCK_EX);
 		}
 	}
 		
-	public function Gdn_Dispatcher_AfterEnabledApplication_Handler(&$Sender) {
+	public function Gdn_Dispatcher_AfterEnabledApplication_Handler($Sender) {
 		$this->_EnabledApplication = ArrayValue('EnabledApplication', $Sender->EventArguments, 'Dashboard');
+	}
+	
+	public function Setup() {
+		return True;
 	}
 	
 	private function _EnabledApplication() {
 		return $this->_EnabledApplication;
 	}
-	
-	public function Setup() {
-	}
-	
-	
 	
 }
